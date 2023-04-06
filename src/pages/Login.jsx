3e280a87c45d946d9ac195b47d084ede13f2e260
jucroizer <81417866+jucroizer@users.noreply.github.com>
2023-04-06 20:16:48 +0200

    creation de la page login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,43 @@
-function Login(){
-    return <div><h1>Bonjour Login</h1></div>
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+import "../styles/Login.css";
+
+function Login() {
+  return (
+    <div>
+      <Header />
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <form>
+            <div className="input-wrapper">
+              <label for="username">Username</label>
+              <input type="text" id="username" />
+            </div>
+            <div className="input-wrapper">
+              <label for="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label for="remember-me">Remember me</label>
+            </div>
+            {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
+            <Link to="/Profil" className="sign-in-button">
+              Sign In
+            </Link>
+            {/* <!-- SHOULD BE THE BUTTON BELOW --> */}
+            {/* <!-- <button className="sign-in-button">Sign In</button> -->
+          <!--  --> */}
+          </form>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default Login
+export default Login;
